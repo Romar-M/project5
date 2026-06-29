98268f95d7b41c7c51f6a8d99c6aaf4967a724dc
Romar <@>
2026-06-29 19:23:47 +0300

    PJ5

---
+++ habits/tests/test_models.py
@@ -0,0 +1,20 @@
+﻿import pytest
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from habits.models import Habit
+
+@pytest.mark.django_db
+def test_habit_duration_validation():
+    user = User.objects.create_user("testuser", password="testpass")
+    with pytest.raises(ValidationError):
+        h = Habit(user=user, place="дом", time="08:00:00", action="сон", duration=130)
+        h.full_clean()
+
+@pytest.mark.django_db
+def test_related_habit_must_be_pleasant():
+    user = User.objects.create_user("testuser2", password="testpass")
+    pleasant = Habit.objects.create(user=user, place="дом", time="09:00:00", action="чай", is_pleasant=True, duration=30)
+    non_pleasant = Habit.objects.create(user=user, place="офис", time="12:00:00", action="обед", is_pleasant=False, duration=45)
+    with pytest.raises(ValidationError):
+        h = Habit(user=user, place="парк", time="13:00:00", action="прогулка", related_habit=non_pleasant, duration=60)
+        h.full_clean()
